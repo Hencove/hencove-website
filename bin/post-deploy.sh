2a7376fa13b62f8419ca7db79e35711012b2af7d
Jonathan Bell <jonathan@hencove.com>
2025-02-24 19:33:10 -0500

    Switch to verbose output for composer to diagnose auth.json errors

--- bin/post-deploy.sh
+++ bin/post-deploy.sh
@@ -4,4 +4,4 @@ echo "Starting post deploy script..."
 echo "Switch directory to wp-content/"
 cd wp-content
 echo "Installing Composer dependencies..."
-composer install --optimize-autoloader --no-dev --no-progress
+composer install --optimize-autoloader --no-dev --no-progress -v
